fb5807452fa540f501b1bff0b521719946ff561b
Tobias Keller <kellertobs@gmail.com>
2025-05-16 23:37:34 +0100

    correct ref parameter value changed unintentionally

--- usr/parvar.m
+++ usr/parvar.m
@@ -13,25 +13,25 @@ D     = [2; 8];       % [m]
 d     = [1e-2; 3e-3]; % [m]
 etam0 = 1e0;          % [Pas]
 
-% magma ocean parameter variations  (5 simulations)
+% magma ocean parameter variations  (8 simulations)
 D     =  1e6;          % [m]
 d0    = [1e-2 (ref); 1e-1];  % [m]
 etam0 = [1e-1 (ref); 1e+0];  % [Pas]
 Rex   = 1;             % [0.1; 10]
 Delta_drg = 1;         % [1/3; 3]
 
-% additional parameter variations (16 simulations)
+% additional parameter variations (17 simulations)
 N          = 200;    % [100; 300];
 CFL        = 0.5;    % [0.25; 1]
 Delta_cnv  = h;      % [1/3; 3]*h
-Delta_sgr  = 10*d0;  % [3; 10; 30]*d0
+Delta_sgr  = 10*d0;  % [3; 30]*d0
 Rec        = 1;      % [0.1; 10]
 Scx        = 1;      % [10]
 Da         = 0.5;    % [1,0.25]
 gamma      = 1e-3;   % [0,1e-4,1e-2]; D = 1e3;
 closed_bot = 0;      % [1]
 
-% total # simulations: 27+5+16 = 50
+% total # simulations: 27+8+17 = 52
 
 % naming convention for RunIDs
 D = 1e1; d = 1e-2; etam0 = 1e+1;  % => D1_d2_e1 reference run
